7d4f09e9ea556f934f6676c0935364a683e62fbb
Amazing <kelvinmakinde2@gmail.com>
2024-09-13 14:09:39 +0100

    initial commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,15 +7,15 @@ function Hero() {
     return (
 
         < div id='hero' >
-            <div className="hero-content">
-                <div className="hero-left">
-                    <p>Hi</p>
-                    <h1>I'm Kelvin <br /> Website Designer</h1>
-                    <span className='bio'> I am a skilled passionate web designer with experience in creating visually appealing and user-friendly websites</span>
-                    <button> <FaBriefcase /> Hire me</button>
-                </div>
-                <img src={hero} alt="" className='hero_img' />
+            <div className="hero-left">
+
+                <p>Hi, I'm Kelvin</p>
+                <h1>Front-End</h1>
+                <h2>Web Developer</h2>
+                <span> I am a skilled passionate web designer with experience in creating visually appealing and user-friendly websites</span>
+                <button> <FaBriefcase /> Hire me</button>
             </div>
+            <div className="hero-right"><img src={hero} alt="" className='hero_img' /></div>
 
         </div >
     )
