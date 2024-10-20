e8f14665b8f0fa03befdd1d7127c77eec9a57623
Amazing-Tech1 <kelvinmakinde2@gmail.com>
2024-10-20 13:51:18 +0100

    new commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useRef } from 'react';
 import './Hero.css';
 import hero from '../../assets/hero.png';
 import { FaBriefcase } from 'react-icons/fa';
-import cv  from '../../assets/kelvin-Resume.pdf'
 import Typed from 'typed.js';
 
 function Hero() {
     const el = useRef(null);
+    const resumeLink = '/Kelvin-Resume.pdf';
+
 
     useEffect(() => {
         const options = {
@@ -32,10 +33,10 @@ function Hero() {
                 <h2>I am Kelvin</h2>
                 <p ref={el}></p>
                 <span>I am a skilled passionate web designer with experience in creating visually appealing and user-friendly websites.</span>
-                <a href={cv} download><button><FaBriefcase /> My Resume</button></a>
+                <a href={resumeLink} target="_blank" rel="noopener noreferrer" download><button><FaBriefcase /> My Resume</button></a>
 
             </div>
-            
+
             <div className="hero-right">
                 <img src={hero} alt="Hero" className='hero_img' />
             </div>
